abe428aa4d08e27f260d6d374848d05b1cd973f8
IT25101607 <it25101607@my.sliit.lk>
2026-05-18 10:14:24 +0530

    Create Base Project Structure

---
+++ src/test/java/com/car_sale/carsalesystem/CarSaleSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.car_sale.carsalesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSaleSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
